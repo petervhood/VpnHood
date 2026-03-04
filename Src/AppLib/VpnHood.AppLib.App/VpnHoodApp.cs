3a00028668c3171d47056376121a94ba535a5a85
petervhood <peter@vpnhood.com>
2026-03-05 01:20:14 +0330

    [release]

--- Src/AppLib/VpnHood.AppLib.App/VpnHoodApp.cs
+++ Src/AppLib/VpnHood.AppLib.App/VpnHoodApp.cs
@@ -122,8 +122,8 @@ public class VpnHoodApp : Singleton<VpnHoodApp>,
             ipLocationZipData: options.Resources.IpLocationZipData);
         locationService.StateChanged += LocationService_StateChanged;
 
-        //ClientProfileService = new ClientProfileService(Path.Combine(StorageFolderPath, FolderNameProfiles));
-        //Diagnoser.StateChanged += (_, _) => FireConnectionStateChanged();
+        ClientProfileService = new ClientProfileService(Path.Combine(StorageFolderPath, FolderNameProfiles));
+        Diagnoser.StateChanged += (_, _) => FireConnectionStateChanged();
 
         // add a default test public server if not added yet
         //var builtInProfileIds = ClientProfileService.ImportBuiltInAccessKeys(options.AccessKeys);
